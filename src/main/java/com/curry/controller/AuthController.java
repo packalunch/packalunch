2d6684c67af411454d0f9967058eb813ff2f5056
sonic <sadrayan@gmail.com>
2015-03-27 18:23:21 -0700

    auth controller for spring security

---
+++ src/main/java/com/curry/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.curry.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
+
+/**
+ * CurryWithAri
+ * Created by sadra on 12/10/14.
+ */
+@Controller
+public class AuthController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
+
+    /**
+     * Return Principal from Spring Security.
+     *
+     * @param principal
+     * @return
+     */
+    @RequestMapping(value = "/auth/username", method = RequestMethod.GET)
+    public @ResponseBody
+    Principal currentUserName(Principal principal) {
+        return principal;
+    }
+
+}
