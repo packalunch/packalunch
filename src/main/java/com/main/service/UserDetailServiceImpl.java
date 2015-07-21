295f7d52419d1f30cd887c7870bc70449155c9f9
sonic <sadrayan@gmail.com>
2015-07-21 03:35:10 -0400

    userId type check to work with ID (social) and findByEmail  (local) users

--- src/main/java/com/main/service/UserDetailServiceImpl.java
+++ src/main/java/com/main/service/UserDetailServiceImpl.java
@@ -15,8 +15,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
  * PackALunch
  * Created by sadra on 2/2/15.
  */
-public class UserDetailServiceImpl implements UserDetailsService
-{
+public class UserDetailServiceImpl implements UserDetailsService {
 
     static org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger.getLogger(UserDetailServiceImpl.class);
 
@@ -29,27 +28,43 @@ public class UserDetailServiceImpl implements UserDetailsService
 
     /**
      * Loads the user information.
-     * @param username  The username of the requested user.
-     * @return  The information of the user.
-     * @throws org.springframework.security.core.userdetails.UsernameNotFoundException
-     * Thrown if no user is found with the given username.
+     *
+     * @param username The username of the requested user.
+     * @return The information of the user.
+     * @throws org.springframework.security.core.userdetails.UsernameNotFoundException Thrown if no user is found with the given username.
      */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         LOGGER.debug("Loading user by username: " + username);
-        User user = userDao.findOne(Integer.parseInt(username));
-        LOGGER.debug("Found userID:" + user.getId());
+
+        User user = null;
+
+        if (isNumeric(username)) { // find by user id if social user
+            user = userDao.findOne(Integer.parseInt(username));
+        } else { // find by email if local user
+            user = userDao.findByEmail(username);
+        }
 
         if (user == null) {
             LOGGER.debug("no user found: " + username);
             throw new UsernameNotFoundException("No user found with username: " + username);
         }
+        LOGGER.debug("Found userID:" + user.getId());
 
         SocialUserDetail socialUserDetail = SocialUserFactory.build(user);
 
         LOGGER.debug("Returning user details: " + socialUserDetail.toString());
 
         return socialUserDetail;
+    }
+
+    private static boolean isNumeric(String str) {
+        try {
+            Integer integer = Integer.parseInt(str);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
     }
 }
